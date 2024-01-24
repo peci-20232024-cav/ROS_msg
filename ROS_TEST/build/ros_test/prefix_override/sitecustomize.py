3e817f382585c993c5b99728af0bb0f88d0cbe98
Pedrinho-Dev-01 <dpedrinho01@gmail.com>
2024-01-24 13:41:13 +0000

    Control ROS Test

---
+++ ROS_TEST/build/ros_test/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/nap/Desktop/ROS_TEST/install/ros_test'
---
+++ ROS_TEST/src/ros_test/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = 'ros_test'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='nap',
+    maintainer_email='nap@todo.todo',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'talker = ros_test.publisher_member_function:main',
+            'listener = ros_test.subscriber_member_function:main',
+        ],
+    },
+)
