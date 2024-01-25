cd161f11ec3ab8c2fba554260bb6f5e124e02e59
Pedrinho-Dev-01 <dpedrinho01@gmail.com>
2024-01-25 13:14:45 +0000

    ros2 topics

--- ROS_TEST/build/turtlesim/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nap/Desktop/ROS_TEST/src/ros_tutorials/turtlesim
-# Build directory: /home/nap/Desktop/ROS_TEST/build/turtlesim
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("turtlesim__py")
