8fd6a1bd80e705ebe14bdb6017f6614db8ce162e
Pedrinho-Dev-01 <dpedrinho01@gmail.com>
2024-01-25 11:48:26 +0000

    ros topics

--- build/semaforo/build/lib/semaforo/subscriber_member_function.py
+++ build/semaforo/build/lib/semaforo/subscriber_member_function.py
@@ -14,43 +14,42 @@
 
 import rclpy
 from rclpy.node import Node
-from rclpy.qos import QoSProfile, QoSDurabilityPolicy
 
 from std_msgs.msg import String
-from tier4_planning_msgs.msg import LateralOffset
 
-test_profile = QoSProfile(depth=10, durability=QoSDurabilityPolicy.TRANSIENT_LOCAL)
+from autoware_auto_perception_msgs.msg import TrafficSignalArray, TrafficSignal
 
-class MinimalPublisher(Node):
+class MinimalSubscriber(Node):
 
     def __init__(self):
-        super().__init__('minimal_publisher')
-        
-        self.publisher_ = self.create_publisher(LateralOffset, '/planning/scenario_planning/lane_driving/behavior_planning/behavior_path_planner/input/lateral_offset', test_profile)
-        timer_period = 0.01  # seconds
-        self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.i = 0
+        super().__init__('minimal_subscriber')
+        self.subscription = self.create_subscription(
+            TrafficSignalArray(),
+            '/perception/traffic_light_recognition/traffic_signals',
+            self.listener_callback,
+            10)
 
-    def timer_callback(self):
-
-        msg = LateralOffset()
-        msg.lateral_offset = 0.1 
-        self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.lateral_offset)
-        self.i += 1
+    def listener_callback(self, msg):
+        for sinal in msg.signals:
+            print("Signal ID: %s \n" % str(sinal.map_primitive_id))
+            for luz in sinal.lights:
+                print("Light Color: %s" % str(luz.color))
+                print("Light Shape: %s" % str(luz.shape))
+                print("Light Status: %s" % str(luz.status))
+                print("Light Confidence: %s \n" % str(luz.confidence))
 
 
 def main(args=None):
     rclpy.init(args=args)
 
-    minimal_publisher = MinimalPublisher()
+    minimal_subscriber = MinimalSubscriber()
 
-    rclpy.spin(minimal_publisher)
+    rclpy.spin(minimal_subscriber)
 
     # Destroy the node explicitly
     # (optional - otherwise it will be done automatically
     # when the garbage collector destroys the node object)
-    minimal_publisher.destroy_node()
+    minimal_subscriber.destroy_node()
     rclpy.shutdown()
 
 
